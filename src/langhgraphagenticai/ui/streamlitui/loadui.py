960eaf0b8f17f9b29689da29f8f602ba0db3f9da
venukashoju <venumadhav.k27@gmail.com>
2025-11-27 19:01:37 +0530

    commit

--- src/langhgraphagenticai/ui/streamlitui/loadui.py
+++ src/langhgraphagenticai/ui/streamlitui/loadui.py
@@ -2,8 +2,25 @@ import streamlit as st
 import os
 
 from src.langhgraphagenticai.ui.uiconfigfile import Config
+# from ..uiconfigfile import Config
+
 
 class LoadStreamlitUI:
     def __init__(self):
         self.config=Config()
         self.user_controls={}
+
+    def load_streamlit_ui(self):
+        st.set_page_config(page_title="bot" + self.config.get_page_title(),layout="wide")
+        st.header("bot " + self.config.get_page_title())
+
+        with st.sidebar:
+            # Get options from congig
+            llm_options = self.config.get_llm_options()
+            usecase_options = self.config.get_usecase_options()
+
+        # LLM selection
+        self.user_controls['selected_llm']=st.selectbox('Select LLM', llm_options)
+
+        if self.user_controls['selected_llm']=='Groq':
+            model_options=self.config.get_groq_model_options()
--- src/langhgraphagenticai/ui/uiconfigfile.py
+++ src/langhgraphagenticai/ui/uiconfigfile.py
@@ -13,3 +13,6 @@ class Config:
     
     def get_groq_model_options(self):
         return self.config['DEFAULT'].get('GROQ_MODEL_OPTIONS').split(", ")
+    
+    def get_page_title(self):
+        return self.config["DEFAULT"].get('GET_PAGE_TITLE').split(', ')
